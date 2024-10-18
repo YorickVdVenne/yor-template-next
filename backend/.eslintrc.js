124ecf34f06b54a31cd2a24b7d05817f7bd4f162
Yorick-Level-Level <yorick@level-level.com>
2024-10-18 21:50:02 +0200

    fix: i dont know anymore

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,12 +1,22 @@
 module.exports = {
   env: {
-    node: true,
-    es2021: true
+    browser: true,
+    es2021: true,
+    node: true
   },
   extends: ['eslint:recommended', 'plugin:prettier/recommended'],
   parserOptions: {
-    ecmaVersion: 12,
+    ecmaVersion: 'latest',
     sourceType: 'module'
   },
-  rules: {}
+  ignorePatterns: ['dist/', 'node_modules/', 'types/generated/'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx', '*.jsx'],
+      parser: '@typescript-eslint/parser',
+      plugins: ['@typescript-eslint'],
+      extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+      rules: {}
+    }
+  ]
 };
